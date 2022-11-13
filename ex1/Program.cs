55388c602529cc3328228544e4702374d4247718
Svetlana <iilsojar@gmail.com>
2022-11-13 10:17:44 +0100

    факториал или напишите программу которая на вход принимает число N и выдает произведение чисел от 1 до N

---
+++ ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿//напишите программу которая на вход принимает число N и выдает произведение чисел от 1 до N
+//4->24
+//5 -> 120
+
+Console.Clear();
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"произведение чисел от 1 до {number} -> {factorial}");
+
+int Factorial (int num)
+{
+    if(num < 0) num = - num;
+    int factor = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        factor = factor * i;
+    }
+    return factor;
+}
+
